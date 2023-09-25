94c3a0e8b3e8ec869f47a94574dcca944084b99d
BlackkTurtle <113180669+BlackkTurtle@users.noreply.github.com>
2023-09-25 21:30:08 +0300

    Initial commit

---
+++ src/main/java/org/planning/freemaker/Lab3SpringApplication.java
@@ -0,0 +1,13 @@
+package org.planning.freemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/planning/freemaker/Lab3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.planning.freemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
